f6873529fca6d00162df1204ebb4454217e9efa8
ollynov <ollynov@gmail.com>
2020-04-25 00:35:43 -0700

    started adding state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
+
+class UserModel {
+  String name = "YoshCode"
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Mentor'),),
-        body: MyFirstWidget(color: Colors.blue),
-        floatingActionButton: FloatingActionButton(),
-        drawer: Drawer(),
+    return Provider<UserModel>.value(
+      value: UserModel(),
+      child: MaterialApp(
+        routes: {
+          '/home': (context) => HomeScreen(),
+          '/slideshow': (context) => SlideshowScreen()
+        },
+        home: HomeScreen(),
       ),
     );
   }
 }
 
-class MyFirstWidget extends StatefulWidget {
-  final Color color;
-
-  MyFirstWidget({Key key, this.color = Colors.red}) : super(key: key);
-
+class HomeScreen extends StatelessWidget {
   @override
-  _MyFirstWidgetState createState() => _MyFirstWidgetState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text('push'),
+              color: Colors.green,
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => SlideshowScreen(name: 'Jeff'))
+                // );
+
+                //  Navigator.pushNamed(
+                //     context,
+                //     '/slideshow'
+                //   );
+
+                Navigator.pushNamed(context, '/slideshow');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyFirstWidgetState extends State<MyFirstWidget> {
-  int count = 0;
-
+class SlideshowScreen extends StatelessWidget {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
   Widget build(BuildContext context) {
-    return Center(
-      child: FlatButton(
-        child: Text('$count'),
-        onPressed: () {
-          setState(() {
-            count++;
-          });
-        }
-      )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Slideshow'),
+        backgroundColor: Colors.red[200],
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text('push'),
+              color: Colors.green,
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => SlideshowScreen(name: 'Jeff'))
+                // );
+
+                //  Navigator.pushNamed(
+                //     context,
+                //     '/slideshow'
+                //   );
+
+                Navigator.pushNamed(context, '/slideshow');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
